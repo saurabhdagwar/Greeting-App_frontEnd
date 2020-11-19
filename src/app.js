bb199d19b986e44fefa45f2f52192570b219c65f
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-11-19 23:24:31 +0530

    refactor: code by implementing delete and edit option with greeting

--- src/app.js
+++ src/app.js
@@ -8,63 +8,44 @@
  * @since       : 16/11/2020
  *************************************************************************************/
 
+const { SSL_OP_EPHEMERAL_RSA } = require("constants");
+
 const url = `http://localhost:4000/greeting`;
 
 /**
  * @description popup windows for create, update and delete buttons
  * @function createGreetingForm to show createGreeting block
  * @function editGreetingForm to show editGreeting block
- * @function deleteGreetingForm to show deleteGreeting block
  * @function closeForm to close all blocks
  */
 createGreetingForm = () => {
-  let popupWindow = "";
-  document.getElementById("greeting").style.display = "none";
   document.getElementById("greeting").style.display = "block";
-  popupWindow = `<h3>Create Greeting</h3>
+  greetingWindow.innerHTML = `<h3>Create Greeting</h3>
+
+  <label ><b>Name:</b></label>
+  <input type="text" placeholder="Enter Name" autocomplete="off" id="greetingName" required >
+
+  <label ><b>Message:</b></label>
+  <input type="text" placeholder="Enter Message" autocomplete="off" id="greetingMessage" required>
+
+  <button type="button" class="btn" onclick="postGreeting()">Create Greeting</button>
+  <button type="button" class="btn cancel" onclick="closeForm()">Close</button>`;
+  
+};
+editGreetingForm = (id) => {
+  document.getElementById("greeting").style.display = "block";
+  greetingWindow.innerHTML = `<h3>Edit Greeting</h3>
 
   <label for="name"><b>Name:</b></label>
-  <input type="text" placeholder="Enter Name" name="name" id="createGreetingName" required >
+  <input type="text" placeholder="Enter Name" autocomplete="off" id="greetingName" required>
 
   <label for="message"><b>Message:</b></label>
-  <input type="text" placeholder="Enter Message" name="message" id="createGreetingMessage" required>
+  <input type="text" placeholder="Enter Message" autocomplete="off" id="greetingMessage" required>
 
-  <button type="submit" class="btn" onclick="postGreeting()">Create Greeting</button>
+  <button type="button" class="btn" onclick="putGreeting('${id}')" >Update Greeting</button>
   <button type="button" class="btn cancel" onclick="closeForm()">Close</button>`;
-  greetingWindow.innerHTML = popupWindow;
 };
-editGreetingForm = () => {
-  let popupWindow = "";
-  document.getElementById("greeting").style.display = "none";
-  document.getElementById("greeting").style.display = "block";
-  popupWindow = `<h3>Edit Greeting</h3>
 
-  <label for="GreetingID"><b>GreetingID:</b></label>
-  <input type="text" placeholder="Enter GreetingID" id="editGreetingID" required>
-
-  <label for="name"><b>Name:</b></label>
-  <input type="text" placeholder="Enter Name" name="name" id="editGreetingName" required>
-
-  <label for="message"><b>Message:</b></label>
-  <input type="text" placeholder="Enter Message" name="message" id="editGreetingMessage" required>
-
-  <button type="submit" class="btn" onclick="putGreeting()" >Update Greeting</button>
-  <button type="button" class="btn cancel" onclick="closeForm()">Close</button>`;
-  greetingWindow.innerHTML = popupWindow;
-};
-deleteGreetingForm = () => {
-  let popupWindow = "";
-  document.getElementById("greeting").style.display = "none";
-  document.getElementById("greeting").style.display = "block";
-  popupWindow = ` <h3>Delete Greeting</h3>
-
-  <label for="GreetingID"><b>GreetingID:</b></label>
-  <input type="text" placeholder="Enter GreetingID" id="deleteGreetingID" required>
-
-  <button type="submit" onclick="deleteGreeting()" class="btn">Delete Greeting</button>
-  <button type="button" class="btn cancel" onclick="closeForm()">Close</button>`;
-  greetingWindow.innerHTML = popupWindow;
-};
 closeForm = () => {
   document.getElementById("greeting").style.display = "none";
 };
@@ -76,10 +57,11 @@ const printCards = (posts) => {
   let output = "";
   posts.forEach((post) => {
     output += `<div class="card" >
-<textarea  name="G1">Id:- ${post._id}
-Name:- ${post.name}
-Message:- ${post.message}
-Created on:- ${post.createdAt}</textarea>
+<div class="box" name="G1"><a class="greetingBox" onclick="selectWork('${post._id}')">Id:- ${post._id} <br> Name:- ${post.name} <br> 
+Message:- ${post.message} <br> Created on:- ${post.createdAt}</a> <br>
+<button type="submit" class="deleteButton" onclick="deleteGreeting('${post._id}')" ><img src="./assets/delete.png">Delete </button>
+<button class="editButton" onclick="editGreetingForm('${post._id}')"><img src="./assets/edit.png"> Edit </button>
+</div>
 </div>`;
   });
   postsCards.innerHTML = output;
@@ -104,36 +86,40 @@ getGreeting = () => {
  */
 postGreeting = () => {
   let greeting = {
-    name: document.getElementById("createGreetingName").value,
-    message: document.getElementById("createGreetingMessage").value,
+    name: document.getElementById("greetingName").value,
+    message: document.getElementById("greetingMessage").value,
   };
   fetch(url, {
     method: "POST",
     body: JSON.stringify(greeting),
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" }
   })
     .then((data) => {
       console.log(data);
+      
     })
     .catch((err) => {
       console.log(err);
     });
+    closeForm();
+    sleep(2000); 
+    getGreeting();
 };
 
 /**
  * @description update greeting using fetch put method according with localhost and id
  * @function putGreeting show response successful if data is proper
  */
-putGreeting = () => {
+putGreeting = (id) => {
   let greeting = {
-    name: document.getElementById("editGreetingName").value,
-    message: document.getElementById("editGreetingMessage").value,
+    name: document.getElementById("greetingName").value,
+    message: document.getElementById("greetingMessage").value,
   };
-  let url1 = `${url}/${document.getElementById("editGreetingID").value}`;
+  let url1 = `${url}/${id}`;
   fetch(url1, {
     method: "put",
     body: JSON.stringify(greeting),
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" }
   })
     .then((data) => {
       console.log(data);
@@ -141,14 +127,16 @@ putGreeting = () => {
     .catch((err) => {
       console.log(err);
     });
+    closeForm();
+    getGreeting();
 };
 
 /**
  * @description delete greeting using fetch delete method according with Greeting id
  * @function deleteGreeting show response successful if id is present and proper
  */
-deleteGreeting = () => {
-  let url1 = `${url}/${document.getElementById("deleteGreetingID").value}`;
+deleteGreeting = (id) => {
+  let url1 = `${url}/${id}`;
   fetch(url1, { method: "delete" })
     .then((data) => {
       console.log(data);
@@ -156,4 +144,6 @@ deleteGreeting = () => {
     .catch((err) => {
       console.log(err);
     });
+    getGreeting();
 };
+
